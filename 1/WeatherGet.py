d07fd86d231f614c9b1f577287c1edaf0ed9f44a
Anna Kerbel <anna.kerbel@pointrf.com>
2018-01-29 17:53:38 +0200

    initial state

---
+++ 1/WeatherGet.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+length = len(sys.argv)
+if length == 2:
+  city = sys.argv[1]
+  uri = 'https://api.openweathermap.org/data/2.5/weather'
+  r = requests.get(uri, params = {'q':city, 'appid':'c01da1b70aeae026480c40cc5806d322'}).text
+  print(r)
+else:
+  print('Incorrect amount of parameters:',  length-1, '. Example of correct parameters: python WeatherGet.py Larnaca')
