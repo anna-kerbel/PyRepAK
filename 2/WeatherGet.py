1379fac4c7055094144dec351b5e5d822674ee2f
Anna.Kerbel <anna.kerbel>
2018-01-30 17:32:18 +0200

    usage of argparse instead of sys

--- 2/WeatherGet.py
+++ 2/WeatherGet.py
@@ -1,10 +1,9 @@
 import requests
-import sys
-length = len(sys.argv)
-if length == 2:
-  city = sys.argv[1]
-  uri = 'https://api.openweathermap.org/data/2.5/weather'
-  r = requests.get(uri, params = {'q':city, 'appid':'c01da1b70aeae026480c40cc5806d322', 'units':'metric'}).text
-  print(r)
-else:
-  print('Incorrect amount of parameters:',  length-1, '. Example of correct parameters: python WeatherGet.py Larnaca')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('City', help= 'Use city name as an argument')
+city = parser.parse_args().City
+uri = 'https://api.openweathermap.org/data/2.5/weather'
+r = requests.get(uri, params = {'q':city, 'appid':'c01da1b70aeae026480c40cc5806d322', 'units':'metric'}).text
+print(r)
+
